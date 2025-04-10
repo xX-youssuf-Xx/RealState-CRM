9c3783f758461f533e8d8e621198c0352e2d30e0
xX-youssuf-Xx <youssufhosam@gmail.com>
2025-04-10 13:54:14 +0200

    build

--- frontend/dist/assets/funnel-C0YnbX2K.js
+++ frontend/dist/assets/funnel-C0YnbX2K.js
@@ -1,4 +1,4 @@
-import{c}from"./x-Br0VJ280.js";/**
+import{c}from"./x-ByRfET2r.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- frontend/dist/assets/x-ByRfET2r.js
+++ frontend/dist/assets/x-ByRfET2r.js
@@ -1,4 +1,4 @@
-import{r as s}from"./index-BAQHcCZP.js";/**
+import{r as s}from"./index-BVlH6NZv.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
